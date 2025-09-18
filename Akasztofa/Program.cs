b7754a2922f7604033d2801c79e14930c9b66320
Imre <nemethi@kkszki.hu>
2025-09-18 12:02:25 +0200

    Már megtudja mondani, hogy talált-e és hogy hol vannak a talált betűk

--- Akasztofa/Program.cs
+++ Akasztofa/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Security;
 using System.Text;
@@ -15,18 +16,46 @@ namespace Akasztofa
             string[] szavak = { "elso", "alma", "vízibicikli", "kukac" };
             Random rnd = new Random();
             
-            bool talalt = false;
+            bool kitalalva = false;
             string szo = szavak[rnd.Next(szavak.Length)];
             Console.WriteLine(szo);
-            Console.ReadLine(); 
+            int elet = 10;
+           
+            int szohossz = szo.Length;
+            string also = new string('_', szohossz);
+
+            string eddig_talalt_betuk = "";
+            eddig_talalt_betuk += also;
+            string uj = "";
+
+
 
             do
             {
+                
                 Console.WriteLine("Adjon meg egy betűt!: ");
                 string betu = Console.ReadLine();
+              
+                for (int i=0; i < szohossz; i++)
+                {
+                    if (szo[i] == betu[0])
+                    {
+                        uj += betu[0];
+                    }
+                    else
+                    {
+                        uj += eddig_talalt_betuk[i];
+                    }
+
+                }
+
+                Console.WriteLine(uj);
+                eddig_talalt_betuk = uj;
+                uj = "";
+               
 
                 
-            }while (! talalt);
+            }while (! kitalalva);
 
         }
     }
