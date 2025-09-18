628738355e6afc458d04e9fa65f82223bbddd77a
Imre <nemethi@kkszki.hu>
2025-09-18 11:11:55 +0200

    Add project files.

---
+++ Akasztofa/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akasztofa
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string[] szavak = { "elso", "alma", "vízibicikli", "kukac" };
+            Random rnd = new Random();
+            
+            bool talalt = false;
+            string szo = szavak[rnd.Next(szavak.Length)];
+            Console.WriteLine(szo);
+            Console.ReadLine(); 
+
+            do
+            {
+                Console.WriteLine("Adjon meg egy betűt!: ");
+                string betu = Console.ReadLine();
+
+                
+            }while (! talalt);
+        }
+    }
+}
---
+++ Akasztofa/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Akasztofa")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Akasztofa")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27eb307c-0ebd-4724-893e-9656672c8af9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
